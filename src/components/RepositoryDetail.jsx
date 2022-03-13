de0241479392b7c346b95a7dc8fdda4753e63b7f
imran-pavel <imran.pavel@gmail.com>
2022-03-13 12:54:44 +0200

    10.20

--- src/components/RepositoryDetail.jsx
+++ src/components/RepositoryDetail.jsx
@@ -1,39 +1,72 @@
 import React  from 'react';
+import { format } from 'date-fns';
 import { useParams } from 'react-router-native';
-import { FlatList, View, Text, StyleSheet } from 'react-native';
+import { FlatList, View, StyleSheet, Dimensions } from 'react-native';
+
+import { ItemSeparator } from './RepositoryList';
+import Text from './Text';
 
 import useRepository from '../hooks/useRepository';
 import RepositoryItem from './RepositoryItem';
 
+import theme from '../theme';
+
+const ratingWidth = 40;
+const parentPadding = 8;
+const windowWidth = Dimensions.get('window').width;
+const reviewDetailsContainerPaddingLeft = 12;
+
 const styles = StyleSheet.create({
   reviewContainer: {
     display: 'flex',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    padding: parentPadding,
+    paddingTop: 8,
+    width: windowWidth
   },
 
   rating: {
-    
-  },
+    width: ratingWidth,
+    height: 40,
+    borderWidth: 2,
+    borderRadius: ratingWidth / 2,
+    borderColor: theme.colors.primary,
 
-  reviewDetails: {
     display: 'flex',
-    flexDirection: 'column'
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 
-  textDetails: {
+  reviewDetailsContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    paddingTop: 2,
+    paddingLeft: reviewDetailsContainerPaddingLeft,
+  },
 
+  detailsText: {
+    width: 0,
+    paddingTop: 2,
+    minWidth: windowWidth - ratingWidth - parentPadding * 2 - reviewDetailsContainerPaddingLeft
   }
 });
 
 const ReviewItem = ({ review }) => {
   return (
     <View style={styles.reviewContainer}>
-      <Text style={styles.rating}>{review.rating}</Text>
-      <View style={styles.reviewDetails}>
-        <Text>
+      <View style={styles.rating}>
+        <Text color='primary' fontWeight='bold'>{review.rating}</Text>
+      </View>
+      <View style={styles.reviewDetailsContainer}>
+        <Text fontWeight='bold'>
           { review.user.username }
         </Text>
-        <Text style={styles.textDetails}>{review.text}</Text>
+        <Text color='textSecondary'>
+          { format(new Date(review.createdAt), 'dd.MM.yyyy') }
+        </Text>
+        <Text numberOfLines={7} style={styles.detailsText}>{review.text}</Text>
       </View>
     </View>
   );
@@ -42,8 +75,6 @@ const ReviewItem = ({ review }) => {
 const RepositoryDetail = ({ repositories }) => {
   const { id } = useParams();
   const result = useRepository({ id });
-
-  console.log('result', result);
 
   if(!repositories) return null;
 
@@ -55,6 +86,7 @@ const RepositoryDetail = ({ repositories }) => {
       data={result.reviews}
       renderItem={({ item }) => <ReviewItem review={item} />}
       keyExtractor={({ id }) => id}
+      ItemSeparatorComponent={ItemSeparator}
       ListHeaderComponent={() => <RepositoryItem item={repoDetails} showURLBtn={true} url={result.url}/>}
     />
   );
